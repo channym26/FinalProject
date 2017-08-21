2a2c34a7c856bd0348cb367a5e39408f8900b36c
Sagechoi <sagechoi88@gmail.com>
2017-08-21 15:51:19 -0700

    Started and added complete consent game.

---
+++ consent_game.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<head>
+<style>
+div#test{ border:#5AB029 3px solid; padding:10px 40px 40px 40px; background-color:#E5FCE3; width:50%;}
+</style>
+<meta charset="utf-8"/>
+<title>Quiz</title>
+</head>
+<body>
+<h2 id="test_status"></h2>
+<div id="test"></div>
+</body>
+</html>
